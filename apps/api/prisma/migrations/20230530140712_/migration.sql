96799eee94b117620d8d241dabfdcac46d785a51
Benjamin Joseph <88529010+benjaminj26@users.noreply.github.com>
2023-06-03 22:33:49 +0530

    feat(api): completed the schema (#11)
    * Completed Schema

--- apps/api/prisma/migrations/20230530140712_/migration.sql
+++
@@ -1,2 +0,0 @@
--- CreateEnum
-CREATE TYPE "PeriodNumber" AS ENUM ('Period1', 'Period2', 'Period3', 'Period4', 'Period5', 'Period6', 'Period7');
--- apps/api/prisma/migrations/20230530140809_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `periodNumber` to the `TimeTable` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "TimeTable" ADD COLUMN     "periodNumber" "PeriodNumber" NOT NULL;
