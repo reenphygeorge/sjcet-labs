5f27e2e8ef0e5bc6f99a08dba3eae20081f27180
Benjamin Joseph <benjaminjoseph2002@gmail.com>
2023-06-03 22:29:32 +0530

    Completed Schema

--- apps/api/prisma/migrations/20230530134100_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `dayOfTheWeek` on the `CourseMapping` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "CourseMapping" DROP COLUMN "dayOfTheWeek";
