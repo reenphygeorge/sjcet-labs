c475f95b3b9a90abd49a5090ff9be904fd623bd6
Benjamin Joseph <88529010+benjaminj26@users.noreply.github.com>
2023-06-03 22:45:37 +0530

    feat(api): added gender to user table

---
+++ apps/api/prisma/migrations/20230603171122_/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - Added the required column `gender` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('MALE', 'FEMALE');
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "gender" "Gender" NOT NULL;
