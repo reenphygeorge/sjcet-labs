176e0ccc661dcfd9c6ef893cbbae2d19e2f3085a
Benjamin Joseph <benjaminjoseph2002@gmail.com>
2023-06-10 19:20:55 +0530

    feat(api): implemented notification system for reservations

--- apps/api/src/services/reservationService.ts
+++ apps/api/src/services/reservationService.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, ReservationStatus } from '@prisma/client';
+import { NotificationType, PrismaClient, ReservationStatus } from '@prisma/client';
 import { ReservationInfo, ReviewInfo } from '../helpers/types/user';
 
 const prisma = new PrismaClient()
@@ -7,6 +7,34 @@ const reservationCreate = async (reservationInfo: ReservationInfo[]) => {
 	const data = await prisma.reservation.createMany({
 		data: reservationInfo
 	})
+
+	const labNames = reservationInfo.map((reservation) => {
+		return reservation.labId
+	})
+
+	for (const labName of labNames) {
+		const labAdmins = await prisma.lab.findUnique({
+			where: {
+				labName
+			},
+			select: {
+				labAdmins: true
+			}
+		})
+
+		if (labAdmins !== null) {
+			for (const admin of labAdmins.labAdmins) {
+				const heading = "Request For Lab Reservation"
+				await prisma.notifications.create({
+					data: {
+						professorsProfessorId: admin.registerNumber,
+						type: NotificationType.RESERVATION_REQUEST,
+						heading
+					}
+				})
+			}
+		}
+	} 
 
 	return data
 }
@@ -31,7 +59,7 @@ const reservationReview = async (reviewInfo: ReviewInfo[]) => {
 	let count = 0
 
 	for (let i = 0; i < reviewInfo.length; ++i) {
-		await prisma.reservation.update({
+		const reservation = await prisma.reservation.update({
 			data: {
 				status: reservationStatus[i]
 			},
@@ -41,6 +69,24 @@ const reservationReview = async (reviewInfo: ReviewInfo[]) => {
 		})
 
 		count += 1
+		
+		const professorId = reservation.professorId
+
+		let notificationType: NotificationType
+		if (reservation.status === 'APPROVED') {
+			notificationType = NotificationType.RESERVATION_APPROVED
+		} else {
+			notificationType = NotificationType.RESERVATION_REJECTED
+		}
+
+		const heading = "Reservation " + reservation.status 
+		await prisma.notifications.create({
+			data: {
+				professorsProfessorId: professorId,
+				type: notificationType,
+				heading
+			}
+		})
 	}
 	return count
 }
